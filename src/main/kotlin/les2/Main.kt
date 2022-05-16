78a9e76b1c22a8994b53656f1f56c0f347423b01
tihv@automacon.ru <PokerStars1>
2022-05-17 02:04:43 +0300

    Задача про проценты

--- src/main/kotlin/les2/Main.kt
+++ src/main/kotlin/les2/Main.kt
@@ -7,5 +7,10 @@ fun main(){
     val scanner = Scanner(System.`in`)
     val count = scanner.nextLine().trim().toInt()
     val amount = count * 100 //копейки
-    println("Комиссия составила " +amount * 0.75f + " копеек")
+    var com = amount * 0.0075
+    if(com <= 3500.0){
+        println("Комиссия составила " + 3500 + " копеек")
+    }else{
+        println("Комиссия составила $com копеек")
+    }
 }
