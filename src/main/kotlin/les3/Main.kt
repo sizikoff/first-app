50f51cb4e04cdda95c7c6e0e4857f83522dd3168
tihv@automacon.ru <PokerStars1>
2022-05-17 01:50:02 +0300

    Задача про секунды

--- src/main/kotlin/les3/Main.kt
+++ src/main/kotlin/les3/Main.kt
@@ -5,38 +5,28 @@ fun main(){
 }
 
 fun agoToText(second:Int):String{
-    var result = ""
-
-    fun minutes(second: Int):String{
-        var minuteText =""
-        var minute = second / 60
-        when(minute){
-            1,21,31,41,51 -> minuteText="$minute минуту назад"
-            2,3,4,22,23,24,32,33,44,52,53,54 -> minuteText="$minute минуты назад"
-            else -> minuteText="$minute минут назад"
-        }
-        return minuteText
-    }
-
-    fun hours(seconds:Int):String{
-        var hours = seconds / 3600
-        var hoursText = ""
-        when(hours){
-            1,21 -> hoursText="$hours час назад"
-            2,3,4,22,23 -> hoursText = "$hours часа назад"
-            else -> hoursText="$hours часов назад"
-        }
-        return hoursText
-    }
-
     when(second){
-        in 0..60 -> result ="только что"
-        in 61..3600 -> result = minutes(second)
-        in 3601..86400 -> result = hours(second)
-        in 86400..172800 -> result = "сегодня"
-        in 172801..259200 -> result = "вчера"
-        else -> "давно"
+        in 0..60 -> return "только что"
+        in 61..3600 -> return minutes(second)
+        in 3601..86400 -> return hours(second)
+        in 86400..172800 -> return "сегодня"
+        in 172801..259200 -> return "вчера"
+        else -> return "давно"
     }
-
-    return result
+}
+fun minutes(second: Int):String{
+    var minute = second / 60
+    when(minute % 10){
+        1 -> return "$minute минуту назад"
+        2,3,4 -> return "$minute минуты назад"
+        else -> return "$minute минут назад"
+    }
+}
+fun hours(seconds:Int):String{
+    var hours = seconds / 3600
+    when(hours % 10){
+        1 -> return "$hours час назад"
+        2,3,4 -> return "$hours часа назад"
+        else -> return "$hours часов назад"
+    }
 }
