7ea5ec255c448e8832e6bf959b48870045691993
tihv@automacon.ru <PokerStars1>
2022-05-16 19:49:23 +0300

    Задача про секунды

--- src/main/kotlin/les3/Main.kt
+++ src/main/kotlin/les3/Main.kt
@@ -1,36 +1,41 @@
 package les3
 
 fun main(){
-    println(agoToText(120322))
+    println(agoToText(2000))
 }
 
 fun agoToText(second:Int):String{
     var result = ""
-    var minuteText =""
-    var hoursText =""
-    var minute = second / 60
-    var hours = minute / 60
 
-    when(minute){
-        1,21,31,41,51 -> minuteText="минуту"
-        2,3,4,22,23,24,32,33,44,52,53,54 -> minuteText="минуты"
-        else -> minuteText="минут"
+    fun minutes(second: Int):String{
+        var minuteText =""
+        var minute = second / 60
+        when(minute){
+            1,21,31,41,51 -> minuteText="$minute минуту назад"
+            2,3,4,22,23,24,32,33,44,52,53,54 -> minuteText="$minute минуты назад"
+            else -> minuteText="$minute минут назад"
+        }
+        return minuteText
     }
 
-    when(hours){
-        1,21 -> hoursText="час"
-        2,3,4,22,23 -> hoursText="часа"
-        else -> hoursText="часов"
+    fun hours(seconds:Int):String{
+        var hours = seconds / 3600
+        var hoursText = ""
+        when(hours){
+            1,21 -> hoursText="$hours час назад"
+            2,3,4,22,23 -> hoursText = "$hours часа назад"
+            else -> hoursText="$hours часов назад"
+        }
+        return hoursText
     }
 
     when(second){
         in 0..60 -> result ="только что"
-        in 61..3600 -> result ="$minute $minuteText назад"
-        in 3601..86400 -> result ="$hours $hoursText назад"
+        in 61..3600 -> result = minutes(second)
+        in 3601..86400 -> result = hours(second)
         in 86400..172800 -> result = "сегодня"
         in 172801..259200 -> result = "вчера"
         else -> "давно"
-
     }
 
     return result
