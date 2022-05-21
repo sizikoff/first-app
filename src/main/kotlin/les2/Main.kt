6a69fdb010c5cf75eea20fc1dadb8ec134e73970
tihv@automacon.ru <PokerStars1>
2022-05-21 10:42:59 +0300

    Задача про копейки

--- src/main/kotlin/les2/Main.kt
+++ src/main/kotlin/les2/Main.kt
@@ -6,11 +6,12 @@ fun main(){
     println("Введите сумму перевода")
     val scanner = Scanner(System.`in`)
     val count = scanner.nextLine().trim().toInt()
-    val amount = count * 100 //копейки
-    var com = amount * 0.0075
-    if(com <= 3500.0){
-        println("Комиссия составила " + 3500 + " копеек")
+    val amount = count * 100//копейки
+    var com:Double
+    if((amount * 0.0075) <= 3500){
+        com = 3500.0
     }else{
-        println("Комиссия составила $com копеек")
+        com = amount * 0.0075
     }
+    println("Комиссия составила $com копеек")
 }
